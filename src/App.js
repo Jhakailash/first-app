d395addbd89827fbc72b15ebf867b3fd43a2b9a1
Jhakailash <kailashjhaji7@gmail.com>
2022-05-18 23:04:57 +0530

    first-app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn kailash React
         </a>
       </header>
     </div>
